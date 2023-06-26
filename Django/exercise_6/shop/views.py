e6df4381ffbc33aae9d92f7efff887e20f26bf5a
mkrol1997 <122916463+mkrol1997@users.noreply.github.com>
2023-06-26 11:31:13 +0200

    Update views names

--- Django/exercise_6/shop/views.py
+++ Django/exercise_6/shop/views.py
@@ -8,7 +8,7 @@ NAMES = ['Connor Pope', 'Emery Hanson', 'Delilah Reyes', 'Kareem Arroyo',
          'Maleah Drake', 'Eliana Cannon', 'Jax Hess', 'Izabelle Fletcher']
 
 
-def GenerateCustomer(request):
+def generate_customer(request):
     if request.method == 'POST':
         Customer.objects.create(name=choice(NAMES))
         return redirect('homepage')
