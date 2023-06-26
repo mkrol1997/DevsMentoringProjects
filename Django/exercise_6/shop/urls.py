e51871b3f9b9b0a74e3122a54582e355470c031a
mkrol1997 <122916463+mkrol1997@users.noreply.github.com>
2023-06-26 11:32:35 +0200

    Update views names

--- Django/exercise_6/shop/urls.py
+++ Django/exercise_6/shop/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import GenerateCustomer
+from .views import generate_customer
 
 urlpatterns = [
-    path('generate_customer/', GenerateCustomer, name='homepage'),
+    path('generate_customer/', generate_customer, name='homepage'),
 ]
